ffd4d9e6ec859230e0ef36947a441411b1cf269b
Goose <mifistosilver@ya.ru>
2020-06-25 17:59:23 +0700

    Сделаны поля (всплывающий календарь и диапазон дат)

--- UI-Kit/scripts/masks.js
+++ UI-Kit/scripts/masks.js
@@ -1,3 +1,13 @@
+//маска даты
 $(function(){
   $('.date').mask('00.00.0000', {placeholder: "ДД.ММ.ГГГГ"});
 });
+
+
+
+
+//ограничение выбора даты для filter date dropdown
+let nowaday = new Date;
+let maxday = new Date(nowaday.getFullYear()+1, nowaday.getMonth(), nowaday.getDate()-1);
+
+$('.filterdatepicker').datepicker({minDate: nowaday, maxDate: maxday});
